6e70a4219e0c8cf61994ae4bc0ccb4853c670de7
Rakesh Gorre <rakeshrakeshgorre@gmail.com>
2026-06-02 12:28:02 +0530

    Switch to using API_URL from environment variables

--- vendora-frontend-main/src/api/userApi.js
+++ vendora-frontend-main/src/api/userApi.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = "http://localhost:8082/api/user";
+// const API = "http://localhost:8082/api/user";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const getProjects = () => {
   return axios.get(API + "/projects");
