e5d16db1ada9ab92b90f2438bae02c3d3356d7af
Rakesh Gorre <rakeshrakeshgorre@gmail.com>
2026-06-02 12:29:49 +0530

    Update API URL definition in userApi.js

--- vendora-frontend-main/src/api/userApi.js
+++ vendora-frontend-main/src/api/userApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API = "http://localhost:8082/api/user";
-const API_URL = import.meta.env.VITE_API_URL;
+const API = "http://localhost:8082/api/user";
+
 
 export const getProjects = () => {
   return axios.get(API + "/projects");
